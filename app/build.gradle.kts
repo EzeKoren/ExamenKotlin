39f29728ed4f3b57f42b2f37a86e8dbccefc993b
Paula <paulyta1983@gmail.com>
2023-11-02 22:25:46 -0300

    creación de boilerplate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    val nav_version = "2.5.3"
+
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 }
--- app/src/main/java/com/yaundeCode/examenAdopcionApp/MainActivity.kt
+++ app/src/main/java/com/yaundeCode/examenAdopcionApp/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.myapplication
+package com.yaundeCode.examenAdopcionApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
