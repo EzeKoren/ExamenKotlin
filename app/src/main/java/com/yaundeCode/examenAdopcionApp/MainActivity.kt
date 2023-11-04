c137a3a5189a85e5392396eea7b54ca8899f5dd5
Ezequiel Korenblum <ezequiel@losko.com.ar>
2023-11-04 16:41:27 -0300

    Resolución de problemas

--- app/src/main/java/com/yaundeCode/examenAdopcionApp/MainActivity.kt
+++ app/src/main/java/com/yaundeCode/examenAdopcionApp/MainActivity.kt
@@ -7,16 +7,10 @@ import android.widget.TextView
 import android.os.PersistableBundle
 import android.view.MenuItem
 import android.widget.Toast
-import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
-
-
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -39,12 +33,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
-
-        val nombre = intent.getStringExtra("nombre")
-        val textViewNombre = findViewById<TextView>(R.id.textViewNombre)
-        textViewNombre.text = nombre
     }
-
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
