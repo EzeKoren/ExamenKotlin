f033c6156bac84d998e17a54b7a98faa9bf22c9f
Federico Peirano <fedepr2345@gmail.com>
2023-11-06 02:19:26 -0300

    Diseño del filtro completo, falta el correcto diseño del buscador

--- app/src/main/java/com/yaundeCode/examenAdopcionApp/fragments/HomeFragment.kt
+++ app/src/main/java/com/yaundeCode/examenAdopcionApp/fragments/HomeFragment.kt
@@ -4,40 +4,67 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.yaundecode.examenadopcionapp.R
 
 class HomeFragment : Fragment() {
+
+    private lateinit var filterFecha: AppCompatButton
+    private lateinit var filterUbicacion: AppCompatButton
+    private lateinit var filterRaza: AppCompatButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        val filterFecha = view.findViewById<RelativeLayout>(R.id.filterFecha)
-        val fitlerFechaText = view.findViewById<TextView>(R.id.fitlerFechaText)
-        val filterPublicacion = view.findViewById<RelativeLayout>(R.id.filterPublicacion)
-        val fitlerPublicacionText = view.findViewById<TextView>(R.id.fitlerPublicacionText)
-        val filterRaza = view.findViewById<RelativeLayout>(R.id.filterRaza)
-        val fitlerRazaText = view.findViewById<TextView>(R.id.fitlerRazaText)
+        filterFecha = view.findViewById(R.id.filterFecha)
+        filterUbicacion = view.findViewById(R.id.filterUbicacion)
+        filterRaza = view.findViewById(R.id.filterRaza)
 
-        filterFecha.setOnClickListener {
-            filterFecha.setBackgroundResource(R.drawable.selector_button_background)
-            fitlerFechaText.setTextColor(requireContext().resources.getColor(R.color.background))
-        }
-        filterFecha.setOnClickListener {
-            filterPublicacion.setBackgroundResource(R.drawable.selector_button_background)
-            fitlerPublicacionText.setTextColor(requireContext().resources.getColor(R.color.background))
-        }
-        filterFecha.setOnClickListener {
-            filterRaza.setBackgroundResource(R.drawable.selector_button_background)
-            fitlerRazaText.setTextColor(requireContext().resources.getColor(R.color.background))
-        }
+        setToggleOnClickListener(filterFecha)
+        setToggleOnClickListener(filterUbicacion)
+        setToggleOnClickListener(filterRaza)
+
         return view
     }
+
+    private fun setToggleOnClickListener(button: AppCompatButton) {
+        button.setOnClickListener {
+            resetButtons() // Restablece el estado de todos los botones
+
+            button.isSelected = true // Selecciona el botón actual
+            button.setBackgroundResource(R.drawable.button_background_press)
+            button.setTextColor(requireContext().resources.getColor(R.color.background))
+        }
+        /*
+        button.setOnClickListener {
+            button.isSelected = !button.isSelected
+            if (button.isSelected) {
+                button.setBackgroundResource(R.drawable.button_background_press)
+                button.setTextColor(requireContext().resources.getColor(R.color.background))
+            } else {
+                button.setBackgroundResource(R.drawable.button_background_nopress)
+                button.setTextColor(requireContext().resources.getColor(R.color.text))
+            }
+        }
+        */
+    }
+    private fun resetButtons() {
+        filterFecha.isSelected = false
+        filterFecha.setBackgroundResource(R.drawable.button_background_nopress)
+        filterFecha.setTextColor(requireContext().resources.getColor(R.color.text))
+
+        filterUbicacion.isSelected = false
+        filterUbicacion.setBackgroundResource(R.drawable.button_background_nopress)
+        filterUbicacion.setTextColor(requireContext().resources.getColor(R.color.text))
+
+        filterRaza.isSelected = false
+        filterRaza.setBackgroundResource(R.drawable.button_background_nopress)
+        filterRaza.setTextColor(requireContext().resources.getColor(R.color.text))
+    }
+
 }
